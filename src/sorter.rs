83cc5e9215b396eb307a11cfb196c041899c0a85
jbrilha <j.brilha@campus.fct.unl.pt>
2024-11-20 14:08:13 +0000

    fix: lexographic order in sorting
    before:
    ...
    README.md
    pdf.pdf
    ...
    
    now:
    ...
    pdf.pdf
    README.md
    ...

--- src/sorter.rs
+++ src/sorter.rs
@@ -9,22 +9,27 @@ pub enum Sorter {
 impl Sorter {
     pub fn paths(&self, a: &PathBuf, b: &PathBuf) -> Ordering {
         match self {
-            Sorter::DirsFirst => {
-                match (a.is_dir(), b.is_dir()) {
-                    (true, true) | (false, false) => a.file_name().cmp(&b.file_name()),
-                    (true, false) => Ordering::Less,
-                    (false, true) => Ordering::Greater,
-
+            Sorter::DirsFirst => match (a.is_dir(), b.is_dir()) {
+                // using just cmp on file_names compares lexographically
+                // meaning that uppercase files would come before lowercase
+                (true, true) | (false, false) => {
+                    a.file_name()
+                        .unwrap()
+                        .to_string_lossy()
+                        .to_lowercase()
+                        .cmp(&b.file_name()
+                            .unwrap()
+                            .to_string_lossy()
+                            .to_lowercase())
                 }
+                (true, false) => Ordering::Less,
+                (false, true) => Ordering::Greater,
             },
-            Sorter::FilesFirst => {
-                match (a.is_dir(), b.is_dir()) {
-                    (true, true) | (false, false) => a.file_name().cmp(&b.file_name()),
-                    (true, false) => Ordering::Greater,
-                    (false, true) => Ordering::Less,
-
-                }
-            }
+            Sorter::FilesFirst => match (a.is_dir(), b.is_dir()) {
+                (true, true) | (false, false) => a.file_name().cmp(&b.file_name()),
+                (true, false) => Ordering::Greater,
+                (false, true) => Ordering::Less,
+            },
             Sorter::Alphabetical => a.file_name().cmp(&b.file_name()),
         }
     }
@@ -36,12 +41,18 @@ impl Sorter {
                 let b_is_dir = b.path().is_dir();
 
                 match (a_is_dir, b_is_dir) {
-                    (true, true) | (false, false) => a.file_name().cmp(&b.file_name()),
+                (true, true) | (false, false) => {
+                    a.file_name()
+                        .to_string_lossy()
+                        .to_lowercase()
+                        .cmp(&b.file_name()
+                            .to_string_lossy()
+                            .to_lowercase())
+                }
                     (true, false) => Ordering::Less,
                     (false, true) => Ordering::Greater,
-
                 }
-            },
+            }
             Sorter::FilesFirst => {
                 let a_is_dir = a.path().is_dir();
                 let b_is_dir = b.path().is_dir();
@@ -50,7 +61,6 @@ impl Sorter {
                     (true, true) | (false, false) => a.file_name().cmp(&b.file_name()),
                     (true, false) => Ordering::Greater,
                     (false, true) => Ordering::Less,
-
                 }
             }
             Sorter::Alphabetical => a.file_name().cmp(&b.file_name()),
